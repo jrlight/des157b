4beb904bb0c7672701890bd1932c4f9ff713062b
jay <jlight413@gmail.com>
2025-04-05 20:35:28 -0700

    portal page 1st draft

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 (function() {
     'use strict';
-
+    const overlay = document.querySelector('#overlay');
     const button = document.querySelector('button');
     const body = document.querySelector('body');
+    const h1 = document.querySelector('h1');
     const banner = document.querySelector('#banner');
     const sections = document.querySelectorAll('section')
     let mode = 'dark';
 
     button.addEventListener('click', function() {
         if (mode === 'dark') {
+            overlay.className = 'switch';
             body.className = 'switch';
+            h1.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
             for (const section of sections) {
@@ -17,7 +20,9 @@
             }
             mode = 'light';
         } else {
+            overlay.removeAttribute('class');
             body.removeAttribute('class');
+            h1.removeAttribute('class');
             banner.removeAttribute('class');
             button.removeAttribute('class');
             for (const section of sections) {
