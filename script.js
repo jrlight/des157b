5473b4768f2d4ac5065a5ca63bc8485256fe23c1
jay <jlight413@gmail.com>
2025-04-06 22:13:08 -0700

    big portal page css update

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 (function() {
     'use strict';
-    const overlay = document.querySelector('#overlay');
-    const button = document.querySelector('button');
+    const mask = document.querySelector('#mask');
+    const lightSwitch = document.querySelector('#lightSwitch');
     const body = document.querySelector('body');
     const h1 = document.querySelector('h1');
     const banner = document.querySelector('#banner');
     const sections = document.querySelectorAll('section')
     let mode = 'dark';
 
-    button.addEventListener('click', function() {
+    //spotlight code (js and css) from: https://blog.finiam.com/blog/spotlight-effect-with-js-and-css
+
+    function handleMouseMove(event) {
+        const { clientX, clientY } = event;
+    
+        mask.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00000000 5px, var(--overlay) 50px)`;
+    }
+    
+    if(mode === 'dark'){
+        document.addEventListener('mousemove', handleMouseMove)
+
+
+    }
+
+    lightSwitch.addEventListener('click', function() {
         if (mode === 'dark') {
-            overlay.className = 'switch';
+            mask.className = 'switch';
             body.className = 'switch';
             h1.className = 'switch';
             banner.className = 'switch';
-            button.className = 'switch';
+            lightSwitch.src = 'images/switch_on.svg';
             for (const section of sections) {
                 section.className = 'switch';
             }
             mode = 'light';
         } else {
-            overlay.removeAttribute('class');
+            mask.removeAttribute('class');
             body.removeAttribute('class');
             h1.removeAttribute('class');
             banner.removeAttribute('class');
-            button.removeAttribute('class');
+            lightSwitch.src = 'images/switch_off.svg';
             for (const section of sections) {
                 section.removeAttribute('class');
             }
