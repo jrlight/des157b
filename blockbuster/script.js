727cb449d162caf4e6b4441efc07da068ac17440
jay <jlight413@gmail.com>
2025-04-15 15:26:25 -0700

    blockbuster finished

--- blockbuster/script.js
+++ blockbuster/script.js
@@ -10,16 +10,19 @@
 
     const lines = document.querySelectorAll('section');
     const lineDisplay = {
-        start: [1,5],
-        stop: [3,7],
+        start: [0.5, 5.85, 9, 10.4, 12, 14.15, 26.75, 30, 33],
+        stop: [4, 7.3, 11, 13.5, 13.5, 19, 29, 31.5, 35],
     }
+    const video = document.querySelector('video');
 
-    const intervalID = setInterval(checkTime, 1000);
+    video.addEventListener('playing', function(){
+        setInterval(checkTime, 1000);
+    });
 
     function checkTime(){
         for(let i = 0; i < lines.length; i++){
-            if(lineDisplay.start[i] < video.currentTime && video.currentTime < overlay.stop[i]){
-                lines[i].className = 'showing';
+            if(lineDisplay.start[i] < video.currentTime && video.currentTime < lineDisplay.stop[i]){
+                lines[i].className = 'showing';                
             }
             else{
                 lines[i].className = 'hidden';
