2b815cc194c07758ffcd6656313cf6485634d4e2
Jay Light <jlight413@gmail.com>
2025-04-30 22:30:28 -0700

    check yourself update- time in seconds added

--- checkYourself/script.js
+++ checkYourself/script.js
@@ -2,6 +2,12 @@ async function getData() {
     const fetchPromise = await fetch('data/laptopAppActivity.json');
     const data = await fetchPromise.json();    
     document.querySelector('#graph').innerHTML = outputHTML(data);
+    const appDivs = await document.querySelectorAll('.appUsed');
+
+    for(let i = 0; i < appDivs.length; i++){
+        appDivs[i].style.flex = data.Apps[i].secondsUsed;
+        appDivs[i].style.background = `#${i}${i}${i}${i}44`;
+    }
 }
 
 function outputHTML(data){
@@ -17,5 +23,5 @@ getData();
 
 (function(){
     'use strict';
-    console.log('reading js');
+    console.log('reading js');    
 })();
