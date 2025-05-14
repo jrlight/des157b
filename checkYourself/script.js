43ec674c17aaf435457e832b456ee5ea18526973
Jay Light <jlight413@gmail.com>
2025-05-13 21:04:54 -0700

    check yourself update

--- checkYourself/script.js
+++ checkYourself/script.js
@@ -10,12 +10,18 @@
     
         for(let i = 0; i < appDivs.length; i++){
             appDivs[i].style.width = `${data.Apps[i].percentUsed}%`;
-            const iMod = i * (50 + i)
-            // appDivs[i].style.background = `rgb(${iMod/2}, ${iMod}, 70)`;
             appDivs[i].style.background = data.Apps[i].color;
         }
 
-        graph.addEventListener('click', handleClick(data));
+        graph.addEventListener('click', handleClick(data));        
+
+        // if(windowDivs.length > 1){
+        //     console.log('haiii');
+            
+        //     for(let i = 0; i < windowDivs.length; i++){
+        //         windowDivs[i].style.background = `rgb(${i}, ${i+2}, 70)`;
+        //     }
+        // }
     }
     
     function outputHTML(data){
@@ -28,17 +34,39 @@
     
     function handleClick(data) {
         const graph = document.querySelector('#graph');
+        let windowGraph = document.querySelector('#windowGraph');
 
         for(let i = 0; i < graph.childNodes.length; i++){
             graph.childNodes[i].addEventListener('click', function(){
+                const popups = document.querySelectorAll('.popup');                
+                if(popups.length > 0){
+                    for(let i = (popups.length - 1); i <= 0; i--){
+                        popups[i].parentNode.removeChild(popups[i]);
+                    }
+                }
                 graph.childNodes[i].innerHTML = `<div class="popup">${data.Apps[i].appName}</h2><p>${data.Apps[i].timeUsed}</p>`;
 
                 if(i === 0 || i === 1){
                     let html = '';
                     for(let j = 0; j < data.Apps[i].windowTitles.length; j++){
                         html += `<div class="windowDiv" title="${data.Apps[i].windowTitles[j].windowName}"></div>`;
-                        console.log(data.Apps[i].windowTitles[j].windowName);
+                        windowGraph.innerHTML = html;
+                        console.log(html);
                     }
+                    
+                    for(let k = 0; k < windowGraph.children.length; k++){
+                        const kMod = k * (50 + k);
+                        windowGraph.children[k].style.background = `rgb(${kMod/2.5}, ${kMod/0.9}, 50)`;
+                        windowGraph.children[k].style.width = `${data.Apps[i].windowTitles[k].percentUsed}%`;
+                    }
+                }
+                else{
+                    // for(let windowDiv of windowGraph.childNodes){
+                    //     windowGraph.removeChild(windowDiv);
+                    // }
+                    windowGraph.childNodes.forEach(element => {
+                        windowGraph.removeChild(element);
+                    });
                 }
             });
         }
