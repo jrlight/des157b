c5bfceb7962b5a53aec0ea5e1473b159058fe51b
Jay Light <jlight413@gmail.com>
2025-06-08 16:08:31 -0700

    content and script updates

--- capstone/script.js
+++ capstone/script.js
@@ -3,4 +3,24 @@
 
     console.log('reading js');
 
+    const accordion = new Accordion(".accordion-container", {
+        duration: 400,
+        showMultiple: true
+    });
+
+    const openAllBtn = document.querySelector('.openAll');
+    const closeAllBtn = document.querySelector('.closeAll');
+
+    openAllBtn.addEventListener('click', function(){
+        accordion.openAll();
+    })
+
+    closeAllBtn.addEventListener('click', function(){
+        accordion.closeAll();
+    })
+
+    console.log(accordion);
+    console.log(accordion.duration);
+    
+
 })();
