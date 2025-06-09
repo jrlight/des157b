4b85b60d1776812f382d70ffe25fc5f9aa42204b
Jay Light <jlight413@gmail.com>
2025-06-09 16:45:01 -0700

    big update an update that's big

--- capstone/script.js
+++ capstone/script.js
@@ -4,27 +4,38 @@
     console.log('reading js');
     
     const vocabList = new Accordion(".vocabList", {
-        duration: 200,
-        showMultiple: true,
-        openOnInit: [0,1,2,3]
-    });
-
-    const accordion = new Accordion(".accordion-container", {
-        duration: 400,
+        duration: 300,
         showMultiple: true
     });
+
+    const vocabWords = document.querySelectorAll('.vocabList .ac-header button');
+
+    for(let i = 0; i < vocabWords.length; i++){        
+        vocabWords[i].addEventListener('click', function(){
+            // if(vocabWords[i].style.background-color === 'var(--flagBlue)'){
+            //     vocabWords[i].style.background-color = 'var(--flagPink)';
+            // } else{
+            //     vocabWords[i].style.background-color = 'var(--flagBlue)';
+            // }
+            vocabWords[i].style.background = 'var(--flagPink)';
+        })
+    }
+    
+
+    // const accordion = new Accordion(".accordion-container", {
+    //     duration: 400,
+    //     showMultiple: true
+    // });
 
     const openAllBtn = document.querySelector('.openAll');
     const closeAllBtn = document.querySelector('.closeAll');
 
     openAllBtn.addEventListener('click', function(){
-        accordion.openAll();
+        vocabList.openAll();
     })
 
     closeAllBtn.addEventListener('click', function(){
-        accordion.closeAll();
-    })
-
-    console.log(accordion);    
+        vocabList.closeAll();
+    })    
 
 })();
