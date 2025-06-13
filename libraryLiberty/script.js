9106201426d132f4ebaeed30006e3a997f88d6f1
Jay Light <jlight413@gmail.com>
2025-06-12 17:37:40 -0700

    library liberty almost finished

--- libraryLiberty/script.js
+++ libraryLiberty/script.js
@@ -2,33 +2,56 @@
     'use strict';
 
     console.log('reading js');
-    
-    const smiley = new Vivus('smiley', {start:'manual', duration:90});
-
-    const smileySvg = document.querySelector('#smiley');
-
-    const drawZone = document.querySelector('#drawZone');
-
-
 
     $('#title').t();
+    
+    
+    const smiley = new Vivus('smiley', {start:'manual', duration:90});
+    const star = new Vivus('star', {start:'manual', duration:90});
+    const exclamation = new Vivus('exclamation', {start:'manual', duration:90});
+
+    let svgIdx = 0;
+
+
+    const smileySvg = document.querySelector('#smiley');
+    const starSvg = document.querySelector('#star');
+    const exclamationSvg = document.querySelector('#exclamation');
+
+    const drawZone = document.querySelector('#drawZone');
+    const svgBtns = document.querySelectorAll('button');
 
     drawZone.addEventListener('click', draw);
+    
+    console.log(svgBtns);
+    
+    for(let i = 0; i < svgBtns.length; i++){
+        svgBtns[i].addEventListener('click', function(){
+            svgIdx = i;
+        });
+    }
 
     function draw(){
-        // drawZone.innerHTML = `<object id="smiley" type="image/svg+xml" data="images/smiley.svg" style="
-        //     transform: translate(-50%, -50%);
-        //     top: ${event.clientY}px;
-        //     left: ${event.clientX}px;
-        // "></object>`
-
-        const test = getComputedStyle(drawZone)
-        console.log(test.getPropertyValue('margin'));
-        
-
-        smileySvg.style.transform = 'translate(-50%, -50%)';
-        smileySvg.style.top = `${event.clientY}px`;            
-        smileySvg.style.left = `${event.clientX}px`;
-        smiley.stop().reset().play();
+        switch(svgIdx){
+            case 0:
+                $(smileySvg).css('transform', 'translate(-50%, -50%)');
+                smileySvg.style.top = `${event.clientY}px`;            
+                smileySvg.style.left = `${event.clientX}px`;
+                smiley.stop().reset().play();
+                break;
+            case 1:
+                $(starSvg).css('transform', 'translate(-50%, -50%)');
+                starSvg.style.top = `${event.clientY}px`;            
+                starSvg.style.left = `${event.clientX}px`;
+                star.stop().reset().play();
+                break;
+            case 2:
+                $(exclamationSvg).css('transform', 'translate(-50%, -50%)');
+                exclamationSvg.style.top = `${event.clientY}px`;            
+                exclamationSvg.style.left = `${event.clientX}px`;
+                exclamation.stop().reset().play();
+                break;
+            default:
+                break;
+        }
     }
 })()
