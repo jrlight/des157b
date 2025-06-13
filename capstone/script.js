7690fb06458faeb029857314c2343bb43fcc1818
Jay Light <jlight413@gmail.com>
2025-06-13 01:22:49 -0700

    CAPSTONE FINAL UPDATE

--- capstone/script.js
+++ capstone/script.js
@@ -12,30 +12,35 @@
 
     for(let i = 0; i < vocabWords.length; i++){        
         vocabWords[i].addEventListener('click', function(){
-            // if(vocabWords[i].style.background-color === 'var(--flagBlue)'){
-            //     vocabWords[i].style.background-color = 'var(--flagPink)';
-            // } else{
-            //     vocabWords[i].style.background-color = 'var(--flagBlue)';
-            // }
             vocabWords[i].style.background = 'var(--flagPink)';
         })
     }
     
 
-    const accordion = new Accordion(".hrtInfo", {
+    const hrtInfo = new Accordion(".hrtInfo", {
         duration: 400,
         showMultiple: true
     });
+
+    const hrtInfoQs = document.querySelectorAll('.hrtInfo .ac-header button');
+
+    for(let i = 0; i < hrtInfoQs.length; i++){        
+        hrtInfoQs[i].addEventListener('click', function(){
+            hrtInfoQs[i].style.background = 'var(--flagPink)';
+        })
+    }
 
     const openAllBtn = document.querySelector('.openAll');
     const closeAllBtn = document.querySelector('.closeAll');
 
     openAllBtn.addEventListener('click', function(){
         vocabList.openAll();
+        hrtInfo.openAll();
     })
 
     closeAllBtn.addEventListener('click', function(){
         vocabList.closeAll();
+        hrtInfo.closeAll();
     })    
 
 })();
